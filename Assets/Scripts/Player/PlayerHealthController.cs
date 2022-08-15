802adb17a8de219f9124224a143543a19b3c9694
Nihar-Desktop <niharkanta1@live.com>
2022-08-15 22:56:54 +0530

    Health UI, Damage and Iframe

--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -10,20 +10,47 @@ Date:       04-08-2022 00:54:49
 ================================================*/
     
 public class PlayerHealthController : MonoBehaviour {
-    public int currentHealth, maxHealth = 3;
+
+    public static PlayerHealthController instance;
+    public int currentHealth, maxHealth = 6;
+    public float iFrameTime;
+
+    private float iFrameCounter;
+    private SpriteRenderer theSR;
+
+    private void Awake() {
+        instance = this;
+    }
 
     private void Start() {
         currentHealth = maxHealth;
+        UIController.instance.UpdateHealthUI();
+
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     private void Update() {
-        
+        if(iFrameCounter >= 0) {
+            iFrameCounter -= Time.deltaTime;
+            if(iFrameCounter <= 0) {
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+            }
+        }
     }
 
     public void DealDamage() {
+        if (iFrameCounter > 0)
+            return;
+        
         currentHealth--;
         if(currentHealth <= 0) {
+            currentHealth = 0;
             gameObject.SetActive(false);
+        }  else {
+            iFrameCounter = iFrameTime;
+            theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 0.5f);
         }
+
+        UIController.instance.UpdateHealthUI();
     }
 }
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*=============================================
+Product:    2DPlatformerMini v1.0
+Developer:  Nihar
+Company:    DWG
+Date:       15-08-2022 22:11:15
+================================================*/
+    
+public class UIController : MonoBehaviour {
+    public static UIController instance;
+    public Image heart1, heart2, heart3;
+    public Sprite heartFull, heartEmpty, heartHalf;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public void UpdateHealthUI() {
+        switch(PlayerHealthController.instance.currentHealth) {
+            case 6:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartFull;
+                break;
+            case 5:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartHalf;
+                break;
+            case 4:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartEmpty;
+                break;
+            case 3:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartHalf;
+                heart3.sprite = heartEmpty;
+                break;
+            case 2:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+            case 1:
+                heart1.sprite = heartHalf;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+
+            default:
+                ClearHealthUI();
+                break;
+        }
+    }
+
+    public void ClearHealthUI() {
+        heart1.sprite = heartEmpty;
+        heart2.sprite = heartEmpty;
+        heart3.sprite = heartEmpty;
+    }
+}
