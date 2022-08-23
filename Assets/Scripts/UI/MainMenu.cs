12555427fdef9d848122e46d1120d304ddff63be
Nihar-Desktop <niharkanta1@live.com>
2022-08-23 23:19:54 +0530

    Main Menu Screen

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*=============================================
+Product:    2DPlatformerMini v1.0
+Developer:  Nihar
+Company:    DWG
+Date:       22-08-2022 22:44:58
+================================================*/
+    
+public class MainMenu : MonoBehaviour {
+
+    public string startScene;
+
+    public void StartGame() {
+        Debug.Log("Start Game...");
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        Debug.Log("Quiting Game.."); 
+    }
+}
